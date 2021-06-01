e68ddd65146661f42b22584efad5100908112437
Edgardo <edgardomartinez182@gmail.com>
2021-06-01 15:16:52 -0600

    added swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1');
+
+  const options = new DocumentBuilder()
+    .setTitle('Book Store')
+    .setDescription('The Book Store API description')
+    .setVersion('1.0')
+    .addTag('books')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api/v1', app, document);
+
   await app.listen(AppModule.port);
 }
 bootstrap();
--- src/modules/user/dto/update-user.dto.ts
+++ src/modules/user/dto/update-user.dto.ts
@@ -1,6 +1,8 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString } from 'class-validator';
 
 export class UpdatedUserDto {
+  @ApiProperty()
   @IsString()
   readonly username: string;
 }
